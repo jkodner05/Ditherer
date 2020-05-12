1692988d2ffde6385f22f245ee7cc1e1b4e15d20
Jordan Kodner <jkodner05@yahoo.com>
2020-05-12 11:08:51 -0400

    updated random and bayer

--- ditherer.py
+++ ditherer.py
@@ -1,6 +1,26 @@
 from PIL import Image
+import imageio
 from random import randint
+import numpy as np
 import sys, getopt
+import argparse
+
+def resize(img, h, w):
+    pic = Image.fromarray(img.astype(np.uint8))
+    pic = pic.resize((h,w),resample=Image.BOX)
+    return np.array(pic).astype(np.int64)
+    
+
+def greyscale(img):
+    img = ((img[:,:,0]+img[:,:,1]+img[:,:,2])/3)
+    np.clip(img,0,255, out=img)
+    return img
+
+
+def downsample(img,cs):
+    img = (img*3/2/cs).astype(np.int64)*cs
+#    np.clip(img,0,255, out=img)
+    return img
 
 def serpentine_fs(num_colors, scale, color, filename, outname):
     image = Image.open(filename)
@@ -41,10 +61,10 @@ def serpentine_fs(num_colors, scale, color, filename, outname):
     image = image.resize((owidth,oheight))
     image.save(outname)
 
-def bayer2x2(num_colors, scale, color, filename, outname):
+def bayer2x2_old(num_colors, scale, color, filename, outname):
     image = Image.open(filename)
     owidth, oheight = image.size
-    image = image.resize((owidth/scale,oheight/scale))
+    image = image.resize((int(owidth/scale),int(oheight/scale)), resample=Image.BOX)
     width, height = image.size
     pic = image.load()
     matrix = ((1,3),(4,2))
@@ -54,15 +74,15 @@ def bayer2x2(num_colors, scale, color, filename, outname):
         pic = image.load()
     for r in range(0,width):
         for c in range(0, height):
-            pic[r,c] = tuple([(val+matrix[r%2][c%2]*num_colors/5)*2/3 for val in pic[r,c]])
+            pic[r,c] = tuple([int((val+matrix[r%2][c%2]*int(num_colors/5))*2/3) for val in pic[r,c]])
             pic[r,c] = palettize(pic[r, c], num_colors)
-    image = image.resize((owidth,oheight))
+    image = image.resize((owidth,oheight), resample=Image.BOX)
     image.save(outname)
 
 def bayer4x4(num_colors, scale, color, filename, outname):
     image = Image.open(filename)
     owidth, oheight = image.size
-    image = image.resize((owidth/scale,oheight/scale))
+    image = image.resize((int(owidth/scale),int(oheight/scale)), resample=Image.BOX)
     width, height = image.size
     pic = image.load()
     matrix = ((0,8,2,10),(12,4,14,6),(3,11,1,9),(15,7,13,5))
@@ -72,16 +92,16 @@ def bayer4x4(num_colors, scale, color, filename, outname):
         pic = image.load()
     for r in range(0,width):
         for c in range(0, height):
-            pic[r,c] = tuple([(val+matrix[r%4][c%4]*num_colors/16)*2/3 for val in pic[r,c]])
+            pic[r,c] = tuple([int((val+matrix[r%4][c%4]*int(num_colors/16))*2/3) for val in pic[r,c]])
             pic[r,c] = palettize(pic[r, c], num_colors)
-    image = image.resize((owidth,oheight))
+    image = image.resize((owidth,oheight), resample=Image.BOX)
     image.save(outname)
 
 
 def floyd_steinberg(num_colors, scale, color, filename, outname):
     image = Image.open(filename)
     owidth, oheight = image.size
-    image = image.resize((owidth/scale,oheight/scale))
+    image = image.resize((int(owidth/scale),int(oheight/scale)), resample=Image.BOX)
     width, height = image.size
     if not color:
         pic = greyscale(image)
@@ -102,7 +122,7 @@ def floyd_steinberg(num_colors, scale, color, filename, outname):
                 pic[x,y+1] = tuple([int(pic[x,y+1][i] + (5.0/16)*error[i]) for i in range(0,3)])
             if x < width - 1:
                 pic[x+1,y] = tuple([int(pic[x+1,y][i] + (7.0/16)*error[i]) for i in range(0,3)])
-    image = image.resize((owidth,oheight))
+    image = image.resize((owidth,oheight), resample=Image.BOX)
     image.save(outname)
 
 def halftone(num_colors, scale, color, filename, outname):
@@ -134,7 +154,7 @@ def halftone(num_colors, scale, color, filename, outname):
 def stucki(num_colors, scale, color, filename, outname):
     image = Image.open(filename)
     owidth, oheight = image.size
-    image = image.resize((owidth/scale,oheight/scale))
+    image = image.resize((int(owidth/scale),int(oheight/scale)), resample=Image.BOX)
     width, height = image.size
     if not color:
         pic = greyscale(image)
@@ -173,7 +193,7 @@ def stucki(num_colors, scale, color, filename, outname):
                 if x < width - 2:
                     pic[x+2,y] = tuple([int(pic[x+2,y][i] + (4.0/42)*error[i]) for i in range(0,3)])
 
-    image = image.resize((owidth,oheight))
+    image = image.resize((owidth,oheight), resample=Image.BOX)
     image.save(outname)
 
 def threshold(num_colors, scale, color, filename, outname):
@@ -192,40 +212,67 @@ def threshold(num_colors, scale, color, filename, outname):
     image = image.resize((owidth,oheight))
     image.save(outname)
 
-def random(num_colors, scale, color, filename, outname):
-    image = Image.open(filename)
-    owidth, oheight = image.size
-    image = image.resize((owidth/scale,oheight/scale))
-    width, height = image.size
-    pic = image.load()
-    for r in range(0,width):
-        for c in range(0, height):
-            pic[r,c] = tuple([val+randint(-160,159) for val in pic[r,c]])
+def random(img):
+    rands = np.random.randint(-160,159,img.shape)
+    img += rands
+    np.clip(img,0,255, out=img)
+    return img
+
+
+def offset(img, offset, cs):
+    w, h, d = img.shape
+    offset = np.tile(offset,(3,1,1)).T
+    bw, bh, bd = offset.shape
+    print(offset.shape, img.shape)
+    print(offset)
+    offset = np.tile(offset,(int(w/bw)+1,int(h/bh)+1,int(d/bd)))[0:w,0:h,0:d]
+    print(offset.shape, img.shape)
+    print(offset)
+    img = (img + offset*int(cs/(bw*bh+1)))*2/3
+    return img
+
+
+def bayer2x2(img, cs):
+    bayer = np.array(((1,3),(4,2)))
+    return offset(img, bayer, cs)
+
+
+def bayer4x4(img, cs):
+    bayer = ((0,8,2,10),(12,4,14,6),(3,11,1,9),(15,7,13,5))
+    return offset(img, bayer, cs)
+
+
+def process(cscale, dimscale, color, infname, outfname, ptype):
+    img = imageio.imread(infname)
+    owidth, oheight, depth = img.shape
+    swidth = int(owidth/dimscale)
+    sheight = int(oheight/dimscale)
+    img = resize(img, sheight, swidth).astype(np.int64)
+
+    if ptype == "threshold":
+        img = img*3/2
+    if ptype == "random":
+        img = random(img)
+    elif ptype == "bayer2x2":
+        img = bayer2x2(img, cscale)
+    elif ptype == "bayer4x4":
+        img = bayer4x4(img, cscale)
+
+    print(img.shape)
     if not color:
-        pic = greyscale(image)
-    else:
-        pic = image.load()
-    for r in range(0,width):
-        for c in range(0, height):
-            pic[r,c] = palettize(pic[r, c], num_colors)
-    image = image.resize((owidth,oheight))
-    image.save(outname)
+        img = greyscale(img.astype(np.int64))
+    print(img.shape)
+    img = downsample(img, cscale)
+    img = resize(img, oheight, owidth)
 
+    imageio.imwrite(outfname,img.astype(np.uint8))
 
-def greyscale(image):
-    width, height = image.size
-    pic = image.load()
-    for x in range(0,width):
-        for y in range(0, height):
-            color = (pic[x,y][0]+pic[x,y][1]+pic[x,y][2])/3
-            pic[x,y] = (color,color,color)
-    return pic
 
 def palettize(pixel, nc):
     """truncate pixel to nearest palette color"""
-    r = int(pixel[0]*3/2) / nc * nc
-    g = int(pixel[1]*3/2) / nc * nc
-    b = int(pixel[2]*3/2) / nc * nc
+    r = int(int(pixel[0]*3/2) / nc) * nc
+    g = int(int(pixel[1]*3/2) / nc) * nc
+    b = int(int(pixel[2]*3/2) / nc) * nc
     return (r,g,b)
 
 def demo(depth, scale, input):
@@ -264,15 +311,15 @@ def demo(depth, scale, input):
 
 
 def help():
-    print "valid processing methods:"
-    print "\tboustrophedonic\n\tfloyd-steinberg\n\tthreshold"
+    print("valid processing methods:")
+    print("\tboustrophedonic\n\tfloyd-steinberg\n\tthreshold")
 
 def output_verbose(depth, scale, color, input, output, type):
-    print "processing image:\t", input, "to", output
-    print "processing method:\t", type
-    print "color:\t\t\t", str(color)
-    print "color depth:\t\t", depth
-    print "resolution:\t\t", "1 /", scale
+    print("processing image:\t", input, "to", output)
+    print("processing method:\t", type)
+    print("color:\t\t\t", str(color))
+    print("color depth:\t\t", depth)
+    print("resolution:\t\t", "1 /", scale)
     return
 
 def main(argv):
@@ -287,7 +334,7 @@ def main(argv):
     try:
         opts, args = getopt.getopt(argv, "i:o:r:d:m:hcvt",["input=", "output=", "resolution=","depth=","method=","help","color","verbose", "demo"])
     except getopt.GetoptError:
-        print "invalid args"
+        print("invalid args")
         help()
         sys.exit(2)
     for opt, arg in opts:
@@ -315,26 +362,24 @@ def main(argv):
     if verbose:
         output_verbose(depth, scale, color, input, output, type)
     if run_demo:
-        demo(255/depth, scale, input)
+        demo(int(255/depth), scale, input)
         sys.exit()
+    process(int(255/depth), scale, color, input, output, type)
+    exit()
     if type == "floyd-steinberg":
-        floyd_steinberg(255/depth, scale, color, input, output)
+        floyd_steinberg(int(255/depth), scale, color, input, output)
     elif type == "threshold":
-        threshold(255/depth, scale, color, input, output)
+        threshold(int(255/depth), scale, color, input, output)
     elif type == "serpentine_fs":
-        serpentine_fs(255/depth, scale, color, input, output)
-    elif type == "random":
-        random(255/depth, scale, color, input, output)
-    elif type == "bayer2x2":
-        bayer2x2(255/depth, scale, color, input, output)
+        serpentine_fs(int(255/depth), scale, color, input, output)
     elif type == "bayer4x4":
-        bayer4x4(255/depth, scale, color, input, output)
+        bayer4x4(int(255/depth), scale, color, input, output)
     elif type == "stucki":
-        stucki(255/depth, scale, color, input, output)
+        stucki(int(255/depth), scale, color, input, output)
     elif type == "halftone":
-        halftone(255/depth, scale, color, input, output)
+        halftone(int(255/depth), scale, color, input, output)
     else:
-        print "Invalid method!"
+        print("Invalid method!")
         help()
         sys.exit(2)
 
